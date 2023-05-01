b4044ce9b1ecb97f212fd395f40173d7edd297b2
FirePhantom303 <46823269+FirePhantom303@users.noreply.github.com>
2023-05-01 22:53:17 +0700

    Update spam_comments.py

--- functions/functions/spam_comments.py
+++ functions/functions/spam_comments.py
@@ -12,7 +12,7 @@ class Function(Base):
 
     def __init__(self, accounts: Field(Types.ACCOUNTS, label_name='Accounts'),
                  posts: Field(Types.INPUTS, label_name='Posts',
-                              pattern = r'^https:\/\/t\.me\/(c\/\d+\/\d+|\w+\/\d+)$',
+                              pattern = r'https:\/\/t\.me\/(c\/\d+\/\d+|\w+\/\d+)',
                               placeholder='https://t.me/simple_post/22122'),
                  comments: Field(Types.TEXTAREA, label_name='Comments',
                                  placeholder='Hello world!'),
